5dc5d4161be2009e39b1f9bd0c6b59df83725d85
Plesker Florian <florian.plesker@web.de>
2024-02-26 22:43:07 +0100

    Further implementation of test cases

--- ng_template_parser/src/parser/parser.test.zig
+++ ng_template_parser/src/parser/parser.test.zig
@@ -5,7 +5,9 @@
 const std = @import("std");
 const Parser = @import("parser.zig").NgTemplateParser;
 const ast = @import("ast.zig");
+const Attribute = @import("token.zig").Attribute;
 const Node = ast.NgTemplateNode;
+const HtmlElement = ast.HtmlElement;
 const expectEqual = std.testing.expectEqual;
 const expect = std.testing.expect;
 
@@ -43,8 +45,21 @@ fn test_equal_nodes(n1: Node, n2: Node) !void {
             std.debug.print("Error: Expected '{any}' recieved '{any}'\n", .{ n1, n2 });
             return err;
         };
-    }
-    if (n1 == .text and n2 == .text) {
+
+        expectEqual(n1.html_element.attributes.items.len, n2.html_element.attributes.items.len) catch |err| {
+            std.debug.print("Error: Expected '{any}' recieved '{any}'\n", .{ n1, n2 });
+            return err;
+        };
+
+        expectEqual(n1.html_element.children.items.len, n2.html_element.children.items.len) catch |err| {
+            std.debug.print("Error: Expected '{any}' recieved '{any}'\n", .{ n1, n2 });
+            return err;
+        };
+
+        for (n1.html_element.children.items, 0..) |item, i| {
+            try test_equal_nodes(item, n2.html_element.children.items[i]);
+        }
+    } else if (n1 == .text and n2 == .text) {
         expect(std.mem.eql(u8, n1.text, n2.text)) catch |err| {
             std.debug.print("Error: Expected '{any}' recieved '{any}'\n", .{ n1, n2 });
             return err;
@@ -68,15 +83,42 @@ test "rawtext" {
 }
 
 test "div" {
+    const content: [:0]const u8 = "<div><div>Hello</div> World<div/></div>";
+
+    var firstDiv = HtmlElement{
+        .name = "div",
+        .self_closing = false,
+        .attributes = std.ArrayListUnmanaged(Attribute){},
+        .children = std.ArrayListUnmanaged(Node){},
+    };
+
+    var secondDiv = HtmlElement{
+        .name = "div",
+        .self_closing = false,
+        .attributes = std.ArrayListUnmanaged(Attribute){},
+        .children = std.ArrayListUnmanaged(Node){},
+    };
+
+    const text1 = Node{ .text = "Hello" };
+    const text2 = Node{ .text = "World" };
+
+    const thirdDiv = HtmlElement{
+        .name = "div",
+        .self_closing = true,
+        .attributes = std.ArrayListUnmanaged(Attribute){},
+        .children = std.ArrayListUnmanaged(Node){},
+    };
+
     const allocator = std.testing.allocator;
-    const content: [:0]const u8 = "<div>Hello World</div>";
+    try firstDiv.children.append(allocator, Node{ .html_element = secondDiv });
+    try firstDiv.children.append(allocator, text2);
+    try firstDiv.children.append(allocator, Node{ .html_element = thirdDiv });
+    defer firstDiv.children.deinit(allocator);
 
-    var parser = Parser.init();
-    var elements = try parser.parse(content, allocator);
+    try secondDiv.children.append(allocator, text1);
+    defer secondDiv.children.deinit(allocator);
 
-    for (elements.items) |*el| {
-        el.deinit(allocator);
-    }
+    var nodes = [_]Node{Node{ .html_element = firstDiv }};
 
-    defer elements.deinit(allocator);
+    try test_equals(content, &nodes);
 }
